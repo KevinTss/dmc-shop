fa96627d44ee1e3507d9ab34c6f6f72b69631447
KevinTss <kevin.tassi@gmail.com>
2025-12-02 13:25:46 +0100

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './app/**/*.{js,jsx,ts,tsx}',
+    './app/root.{js,jsx,ts,tsx}',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
